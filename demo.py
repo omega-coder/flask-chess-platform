3a15bcdb85f51fd2e81c5773f543214ae0f3bd30
omega-coder <fy_cherief@esi.dz>
2019-04-10 16:53:09 +0200

    Adds Player1 and Player2 classes

---
+++ demo.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python3
+
+
+# coding : utf-8
+
+from flask import Flask, Response, request
+import chess, chess.pgn
+
+
+class Player1(Player):
+    pass
+
+class Player2(Player):
+   pass
+
+
+if __name__ == "__main__":
+    start_demo()
+
