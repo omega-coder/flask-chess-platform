76c831ea83e3a557fe06d922cd371947b72bc82e
omega-coder <fy_cherief@esi.dz>
2019-04-19 02:48:44 +0200

    Adds onDragStart event && Separates Config from ChessBoard constructor

--- static/js/app.js
+++ static/js/app.js
@@ -1,4 +1,4 @@
-function onDrop(source, target, piece) {
+function onDrop(source, target, piece, orientation) {
   var pn = piece.includes('b')
     ? piece.toUpperCase().substring(1, 2)
     : piece.substring(1, 2);
@@ -22,26 +22,37 @@ function onDrop(source, target, piece) {
     var m_len = moves.length;
     var row_number = 1;
     while (i < m_len) {
-        var tr = document.createElement("tr");
-        var th = document.createElement("th");
-        th.setAttribute("scope", row_number.toString());
-        th.innerText = row_number.toString();
-        tr.appendChild(th);
-        var td = document.createElement("td");
-        td.innerText = moves[i].toString();
+      var tr = document.createElement('tr');
+      var th = document.createElement('th');
+      th.setAttribute('scope', row_number.toString());
+      th.innerText = row_number.toString();
+      tr.appendChild(th);
+      var td = document.createElement('td');
+      td.innerText = moves[i].toString();
+      tr.appendChild(td);
+      if (m_len % 2 != 1) {
+        var td = document.createElement('td');
+        td.innerText = moves[i + 1].toString();
         tr.appendChild(td);
-        if (m_len % 2 != 1) {
-            var td = document.createElement("td");
-            td.innerText = moves[i+1].toString();
-            tr.appendChild(td);
-        }
-        i += 2;
-        row_number++;
+      }
+      i += 2;
+      row_number++;
 
-        tbody.appendChild(tr);
+      tbody.appendChild(tr);
     }
     board.position(data.fen);
   });
+}
+
+// to fix player with white/black peices from messing arround with other player's pieces.
+// can be bypassed tho., that's why its also validated in back-end too.
+function onDragStart(source, piece, position, orientation) {
+  if (
+    (orientation === 'white' && piece.search(/^w/) === -1) ||
+    (orientation === 'black' && piece.search(/^b/) === -1)
+  ) {
+    return false;
+  }
 }
 
 $('#reset').click(function() {
